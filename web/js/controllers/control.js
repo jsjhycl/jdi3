5fbf4778f9d29c84488ab563be4875671de6ffe1
Scyh <8219017332qq.com>
2019-07-31 15:00:08 +0800

    修复id bug

--- web/js/controllers/control.js
+++ web/js/controllers/control.js
@@ -44,7 +44,7 @@ Control.prototype = {
             default:
                 prefix = "";
                 $nodes = $workspace
-                    .find('.workspace-node:not(.workspace-node[data-type="img"],.workspace-node[data-type="div"]),.workspace-node[data-type="div"] :input');
+                    .find('.workspace-node:not(.workspace-node[data-type="img"],.workspace-node:not(.workspace-node[data-type="arrow"],.workspace-node[data-type="div"]),.workspace-node[data-type="div"] :input');
                 break;
         }
         $nodes.each(function () {

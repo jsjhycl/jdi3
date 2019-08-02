ff5091234b1600280bfee2e35725e55598529245
Scyh <8219017332qq.com>
2019-08-02 11:30:12 +0800

    新增接口

--- web/js/services/service.js
+++ web/js/services/service.js
@@ -1,23 +1,79 @@
 function Service() {
-    this.queryUrl = "/new/table/";
+    this.baseUrl = "/new/table";
 }
 Service.prototype = {
-    request: function (url, data) {
+    base: function (data, callBack) {
         var that = this;
         return new Promise(function (resolve, reject) {
             $.cajax({
-                url: that.queryUrl + url,
+                url: that.baseUrl,
                 type: "POST",
                 data: data,
                 dataType: "json",
-                success: function (result) {
-                    return resolve(result)
+                success: rst => {
+                    if (rst.status === 0) {
+                        callBack && callBack(rst.result)
+                        resolve(rst.result)
+                    } else resolve(rst.result)
                 },
-                error: function (result) {
-                    return reject(result)
-                }
+                error: err => reject(err)
             })
         })
-    }
+    },
+
+    queryCount: function(table, condition) {
+        if (!table) return alert('查询表名不存在！');
+        let config = {
+            command: "query",
+            table: table,
+            condition: condition || [],
+        };
+        return this.base(config);
+    },
+
+    query: function(table, condition, fields, page, size, callBack) {
+        if (!table) return alert('查询表名不存在！');
+        let config = {
+            command: "query",
+            table: table,
+            condition: condition || [],
+            fields: fields || [],
+        };
+        page != undefined && (config['page'] = page);
+        size && (config['size'] = size);
+        
+        return this.base(config, callBack);
+    },
+
+    insert: function(table, save, callBack) {
+        if (!table) return alert("插入表名不存在")
+        let config = {
+            command: "insert",
+            table: table,
+            save: save || []
+        }
+        return this.base(config, callBack);
+    },
+
+    update: function(table, condition, save, callBack) {
+        if (!table || !Array.isArray(condition) || condition.length <= 0) return alert("更新表名或条件不存在！");
+        let config = {
+            command: "update",
+            table: table,
+            condition: condition|| [],
+            save: save || []
+        }
+        return this.base(config, callBack);
+    },
+
+    remove: function(table, condition, callBack) {
+        if (!table || !Array.isArray(condition) || condition.length <= 0) return alert("删除表名或条件不存在！");
+        let config = {
+            command: "remove",
+            table: table,
+            condition: condition|| [],
+        }
+        return this.base(config, callBack);
+    },
 
 }

4b3158bde64b332fea430d08e2d8944438d88517
Scyh <8219017332qq.com>
2019-08-05 18:55:57 +0800

    添加category修改功能

---
+++ web/js/modals/changeCategory.modal.js
@@ -0,0 +1,145 @@
+function ChangeCategoryModal($modal) {
+    BaseModal.call(this, $modal);
+    this.$body = this.$modal.find(".modal-body");
+    this.$select = this.$modal.find('[data-name="selectName"]');
+    this.$option = this.$modal.find('[data-name="setOption"]');
+    this.$add = this.$modal.find('.add');
+    this.cates = null;
+    this.NAME_SPACE = '.ChangeCategoryModal'
+
+    this.renderOptions = function(data) {
+        if (!Array.isArray(data)) return this.$option.empty();
+
+        var html = "",
+            type = null;    // 不是对象，就是字符串
+        data.forEach(item => {
+            !type && (type = DataType.isObject(item));
+            html += this.getOptionItem(type, item);
+        })
+        this.$option.empty().append(html);
+        this.getOptionItem(type, null, true);
+        this.$select.data('type', type);
+    };
+
+    this.getOptionItem = function(type, data, flag = false) {
+        var html = type
+                        ? `
+                        <div class="col-lg-12 option-item">
+                            <div class="form-group">
+                                <label class="col-lg-2 control-label">显示值：</label>
+                                <div class="col-lg-4">
+                                    <input type="text" class="form-control" data-name="name" value="${data ? data.name : ''}">
+                                </div>
+                                <label class="col-lg-2 control-label">实际值：</label>
+                                <div class="col-lg-4">
+                                    <input type="text" class="form-control" data-name="value" value="${data ? data.value : ''}">
+                                </div>
+                                <icon class="delete">X</icon>
+                            </div>
+                        </div>
+                        `
+                        : `
+                            <div class="col-lg-6 option-item">
+                                <div class="form-group">
+                                    <label class="col-lg-4 control-label">显示值：</label>
+                                    <div class="col-lg-8">
+                                        <input type="text" class="form-control" data-name="" value="${data ? data : ''}">
+                                    </div>
+                                    <icon class="delete">X</icon>
+                                </div>
+                            </div>
+                        `
+        flag && this.$option.append(html);
+        return html;
+    }
+}
+
+ChangeCategoryModal.prototype = {
+    initData: async function() {
+        this.clear();
+        await this.setData()
+        this.bindEvents();
+    },
+
+    clear: function() {
+        this.$modal.off(this.NAME_SPACE);
+        this.cates = null;
+        this.$add.hide();
+        this.$option.empty();
+    },
+
+    setData: async function() {
+        var cates = await new FileService().readFile("./profiles/category.json");
+        if (!DataType.isObject(cates)) return alert('获取保存项文件失败！');
+
+        var that = this,
+            options = [];
+        that.cates = cates;
+        options = Object.keys(that.cates).map(i => { return { name: i, value: i }  })
+        Common.fillSelect(this.$select, { name: "请选择修改项", value: "" }, options);
+    },
+
+    saveData: function() {
+            var that = this,
+                hasErr = false;
+            Object.keys(that.cates).map(cate => {
+                var type = DataType.isObject(that.cates[cate]);
+                that.cates[cate] = that.cates[cate].filter(el => type ? (el.name != "" && el.value != "") : el != "" );
+                var data = that.cates[cate];
+                if (Array.isArray(data)) {
+                    data.forEach(el => {
+                        if (el.name == "" || el.value == "") {
+                            alert(cate + ' 存在未填写完整选项');
+                            hasErr = true;
+                            return false;
+                        }
+                    })
+                }
+            })
+            if (hasErr) {
+                return -1
+            }
+            new FileService().writeFile("./profiles/category.json", JSON.stringify(that.cates), function() {
+                Common.fillCategorySelect(that.cates);
+            })
+    },
+
+    bindEvents: function() {
+        var that = this;
+
+        that.$modal.on('change' + that.NAME_SPACE, '[data-name="selectName"]', function(event) {
+            var val = $(this).val();
+            val ? that.$add.show() : that.$add.hide();
+            that.renderOptions(that.cates[val])
+        });
+
+        that.$modal.on('click' + that.NAME_SPACE, '.delete', function(event) {
+            var $parent = $(this).parent().parent(".option-item"),
+                idx = $parent.index(),
+                cate = that.$select.val();
+            if (that.cates[cate] && that.cates[cate][idx]) {
+                that.cates[cate].splice(idx, 1)
+                $parent.remove();
+            };
+        });
+
+        that.$modal.on('click' + that.NAME_SPACE, '.add', function(event) {
+            that.getOptionItem(that.$select.data('type'), null, true);
+        });
+
+        that.$modal.on('input' + that.NAME_SPACE, ".option-item :input", function() {
+            var val = $(this).val(),
+                option_name = $(this).data('name'),
+                $parent = $(this).parents(".option-item"),
+                idx = $parent.index(),
+                cate = that.$select.val(),
+                type = that.$select.data('type');
+            if (!that.cates[cate][idx]) { that.cates[cate][idx] = type ? { name: "", value: "" } : val };
+            option_name && (that.cates[cate][idx][option_name] = val);
+        })
+    },
+
+    execute: function() {
+        this.basicEvents(true, this.initData, this.saveData)
+    }
+}
--- web/js/utils/common.js
+++ web/js/utils/common.js
@@ -38,6 +38,25 @@ var Common = (function () {
                 $select.val(selectedValue);
             }
         },
+        fillCategorySelect: function(result) {
+            var data = result["子分类"].map(function (item) { //遍历result的子分类
+				return {
+					name: item,
+					value: item
+				};
+			});
+			Common.fillRadio($("#model_resource_subCategory"), "model_resource_subCategory", data); //调用Common的fillRadio的资源分类
+			//表单资源提交表单
+			Common.fillSelect($('[name="template_category"]'), null, result["表单分类"], null, true); //填充表单分类下拉框
+			Common.fillRadio($("#template_subCategory"), "template_subCategory", data); //填充表单分类的单选框
+			//布局资源提交表单
+			Common.fillSelect($('[name="model_category"]'), null, result["布局分类"], null, true); //填充布局分类的下拉框
+			Common.fillSelect($('[name="model_userGrade"]'), null, result["布局用户级别"], null, true); //填充用户级别的下拉框
+			Common.fillSelect($('[name="model_feature"]'), null, result["布局特性"], null, true); //填充布局特性的下拉框
+			Common.fillSelect($('[name="model_area"]'), null, result["布局区域"], null, true);
+
+			Common.fillSelect($('[name="model_autoCreate"]'), null, result["自动分表"], null, true); //新增自动分表属性
+        },
         fillRadio: function ($container, name, data) {
             if (!$container || $container.length <= 0) return;
             if (!Array.isArray(data)) {

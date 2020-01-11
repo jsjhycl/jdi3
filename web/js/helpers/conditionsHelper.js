6c24831a52231b4d8bef080a3979644f96e5b145
w-local <1436931552@qq.com>
2020-01-11 17:55:54 +0800

    注销闲置或未实现代码及修改复制属性文本值

--- web/js/helpers/conditionsHelper.js
+++ web/js/helpers/conditionsHelper.js
@@ -3,6 +3,40 @@ var ConditionsHelper = (function () {
         var result = null;
         if (mode === 1) { //适用于数据库查询配置等
             result = [{
+                name: "等于",
+                value: "="
+            },
+            {
+                name: "不等于",
+                value: "<>"
+            },
+            {
+                name: "模糊匹配",
+                value: "like"
+            },
+            {
+                name: "大于",
+                value: ">"
+            },
+            {
+                name: "大于等于",
+                value: ">="
+            },
+            {
+                name: "小于",
+                value: "<"
+            },
+            {
+                name: "小于等于",
+                value: "<="
+            },
+            {
+                name: "存在",
+                value: "in"
+            }
+            ];
+            if (type === "String") {
+                result = [{
                     name: "等于",
                     value: "="
                 },
@@ -13,6 +47,16 @@ var ConditionsHelper = (function () {
                 {
                     name: "模糊匹配",
                     value: "like"
+                }
+                ];
+            } else if (type === "Number") {
+                result = [{
+                    name: "等于",
+                    value: "="
+                },
+                {
+                    name: "不等于",
+                    value: "<>"
                 },
                 {
                     name: "大于",
@@ -29,55 +73,47 @@ var ConditionsHelper = (function () {
                 {
                     name: "小于等于",
                     value: "<="
-                },
-                {
-                    name: "存在",
-                    value: "in"
                 }
-            ];
-            if (type === "String") {
-                result = [{
-                        name: "等于",
-                        value: "="
-                    },
-                    {
-                        name: "不等于",
-                        value: "<>"
-                    },
-                    {
-                        name: "模糊匹配",
-                        value: "like"
-                    }
-                ];
-            } else if (type === "Number") {
-                result = [{
-                        name: "等于",
-                        value: "="
-                    },
-                    {
-                        name: "不等于",
-                        value: "<>"
-                    },
-                    {
-                        name: "大于",
-                        value: ">"
-                    },
-                    {
-                        name: "大于等于",
-                        value: ">="
-                    },
-                    {
-                        name: "小于",
-                        value: "<"
-                    },
-                    {
-                        name: "小于等于",
-                        value: "<="
-                    }
                 ];
             }
         } else if (mode === 2) { //适用于触发条件配置等
             result = [{
+                name: "等于",
+                value: "==="
+            },
+            {
+                name: "不等于",
+                value: "!=="
+            },
+            {
+                name: "大于",
+                value: ">"
+            },
+            {
+                name: "大于等于",
+                value: ">="
+            },
+            {
+                name: "小于",
+                value: "<"
+            },
+            {
+                name: "小于等于",
+                value: "<="
+            }
+            ];
+            if (type === "String") {
+                result = [{
+                    name: "等于",
+                    value: "==="
+                },
+                {
+                    name: "不等于",
+                    value: "!=="
+                }
+                ];
+            } else if (type === "Number") {
+                result = [{
                     name: "等于",
                     value: "==="
                 },
@@ -101,46 +137,42 @@ var ConditionsHelper = (function () {
                     name: "小于等于",
                     value: "<="
                 }
-            ];
-            if (type === "String") {
-                result = [{
-                        name: "等于",
-                        value: "==="
-                    },
-                    {
-                        name: "不等于",
-                        value: "!=="
-                    }
-                ];
-            } else if (type === "Number") {
-                result = [{
-                        name: "等于",
-                        value: "==="
-                    },
-                    {
-                        name: "不等于",
-                        value: "!=="
-                    },
-                    {
-                        name: "大于",
-                        value: ">"
-                    },
-                    {
-                        name: "大于等于",
-                        value: ">="
-                    },
-                    {
-                        name: "小于",
-                        value: "<"
-                    },
-                    {
-                        name: "小于等于",
-                        value: "<="
-                    }
                 ];
             }
         } else if (mode === 3) { //适用于抄送值配置等
             result = [{
+                name: "赋值",
+                value: "="
+            },
+            {
+                name: "自增",
+                value: "+"
+            },
+            {
+                name: "自减",
+                value: "-"
+            },
+            {
+                name: "自乘",
+                value: "*"
+            },
+            {
+                name: "自除",
+                value: "/"
+            }
+            ];
+            if (type === "String") {
+                result = [{
+                    name: "赋值",
+                    value: "="
+                },
+                {
+                    name: "连接",
+                    value: "&"
+                }
+                ];
+            } else if (type === "Number") {
+                result = [{
                     name: "赋值",
                     value: "="
                 },
@@ -160,42 +192,40 @@ var ConditionsHelper = (function () {
                     name: "自除",
                     value: "/"
                 }
-            ];
-            if (type === "String") {
-                result = [{
-                        name: "赋值",
-                        value: "="
-                    },
-                    {
-                        name: "连接",
-                        value: "&"
-                    }
-                ];
-            } else if (type === "Number") {
-                result = [{
-                        name: "赋值",
-                        value: "="
-                    },
-                    {
-                        name: "自增",
-                        value: "+"
-                    },
-                    {
-                        name: "自减",
-                        value: "-"
-                    },
-                    {
-                        name: "自乘",
-                        value: "*"
-                    },
-                    {
-                        name: "自除",
-                        value: "/"
-                    }
                 ];
             }
         } else if (mode === 4) {
             result = [{
+                name: "等于",
+                value: "="
+            },
+            {
+                name: "不等于",
+                value: "<>"
+            },
+            {
+                name: "模糊匹配",
+                value: "like"
+            },
+            {
+                name: "大于",
+                value: ">"
+            },
+            {
+                name: "大于等于",
+                value: ">="
+            },
+            {
+                name: "小于",
+                value: "<"
+            },
+            {
+                name: "小于等于",
+                value: "<="
+            }
+            ];
+            if (type === "String") {
+                result = [{
                     name: "等于",
                     value: "="
                 },
@@ -206,6 +236,16 @@ var ConditionsHelper = (function () {
                 {
                     name: "模糊匹配",
                     value: "like"
+                }
+                ];
+            } else if (type === "Number") {
+                result = [{
+                    name: "等于",
+                    value: "="
+                },
+                {
+                    name: "不等于",
+                    value: "<>"
                 },
                 {
                     name: "大于",
@@ -223,46 +263,6 @@ var ConditionsHelper = (function () {
                     name: "小于等于",
                     value: "<="
                 }
-            ];
-            if (type === "String") {
-                result = [{
-                        name: "等于",
-                        value: "="
-                    },
-                    {
-                        name: "不等于",
-                        value: "<>"
-                    },
-                    {
-                        name: "模糊匹配",
-                        value: "like"
-                    }
-                ];
-            } else if (type === "Number") {
-                result = [{
-                        name: "等于",
-                        value: "="
-                    },
-                    {
-                        name: "不等于",
-                        value: "<>"
-                    },
-                    {
-                        name: "大于",
-                        value: ">"
-                    },
-                    {
-                        name: "大于等于",
-                        value: ">="
-                    },
-                    {
-                        name: "小于",
-                        value: "<"
-                    },
-                    {
-                        name: "小于等于",
-                        value: "<="
-                    }
                 ];
             }
         }
@@ -271,50 +271,50 @@ var ConditionsHelper = (function () {
 
     return {
         typeConfig: [{
-                name: "字符串",
-                value: "String"
-            },
-            {
-                name: "数字",
-                value: "Number"
-            },
-            {
-                name: "元素",
-                value: "Element"
-            },
-            {
-                name: "查询字符串",
-                value: "QueryString"
-            },
-            {
-                name: "变量",
-                value: "outerSideVariable"
-            },
-            {
-                name: "动态数据",
-                value: "notSure"
-            }
+            name: "字符串",
+            value: "String"
+        },
+        {
+            name: "数字",
+            value: "Number"
+        },
+        {
+            name: "元素",
+            value: "Element"
+        },
+        // {
+        //     name: "查询字符串",
+        //     value: "QueryString"
+        // },
+        {
+            name: "变量",
+            value: "outerSideVariable"
+        },
+        {
+            name: "动态数据",
+            value: "notSure"
+        }
         ],
         copySendConfig: [{
-                name: "元素",
-                value: "Element"
-            },
-            {
-                name: "字符串",
-                value: "String"
-            },
-            {
-                name: "数字",
-                value: "Number"
-            },
-            {
-                name: "动态数据",
-                value: "notSure"
-            },
-            {
-                name: "查询字符串",
-                value: "QueryString"
-            },
+            name: "元素",
+            value: "Element"
+        },
+        {
+            name: "字符串",
+            value: "String"
+        },
+        {
+            name: "数字",
+            value: "Number"
+        },
+        {
+            name: "动态数据",
+            value: "notSure"
+        },
+            // {
+            //     name: "查询字符串",
+            //     value: "QueryString"
+            // },
         ],
         reduceTypeConfig: [{
             name: "字符串",
